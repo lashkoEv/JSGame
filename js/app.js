4666f9ee51d3a0c13293639868cf8fbfd0d7028f
Евгения Лашко <smevlk2014@gmail.com>
2023-04-10 23:13:40 +0300

    add main sound theme

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 document.addEventListener("mousemove", (event) => {
   sword.style.left = event.pageX - 120 + "px";
 });
+
+const mainAudio = new Audio();
+mainAudio.src = "../public/sounds/main-theme/main-3.mp3";
+mainAudio.loop = true;
+
+sound.addEventListener("click", () => {
+  if(mainAudio.paused) {
+    soundImg.src = "./public/img/icons/sound-on.png";
+  mainAudio.play();
+  }else {
+    mainAudio.pause();
+    soundImg.src = "./public/img/icons/sound-off.png";
+  }
+  
+});
