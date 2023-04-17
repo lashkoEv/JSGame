08fde08200637de5f8fb84b8faa598935dfc1840
Евгения Лашко <smevlk2014@gmail.com>
2023-04-17 13:32:44 +0300

    save and load progress

---
+++ js/Components/Storage/Storage.js
@@ -0,0 +1,18 @@
+import { Player } from "../Player/Player.js";
+
+export class Storage {
+  save(player) {
+    localStorage.setItem("hp", player.hp);
+    localStorage.setItem("score", player.score);
+  }
+
+  load() {
+    if(localStorage.getItem("hp") && localStorage.getItem("score")) {
+        const hp = +localStorage.getItem("hp");
+        const score = +localStorage.getItem("score");
+        return new Player(hp, score);
+    }
+
+    return new Player();
+  }
+}
--- js/app.js
+++ js/app.js
@@ -7,19 +7,27 @@ import {
 } from "./Components/Init/initSword.js";
 import { Player } from "./Components/Player/Player.js";
 import { Scene } from "./Components/Scene/Scene.js";
+import { Storage } from "./Components/Storage/Storage.js";
 
 const app = () => {
+  const storage = new Storage();
+
   initSwordMovement();
   initSwordAnimation();
   initSwordSound();
 
   initSound();
 
-  const player = new Player(100,1200);
+  const player = storage.load();
 
   initPotion(player);
   
   const scene = new Scene(player);
+
+  setTimeout(() => {
+    storage.save(player);
+    alert("save");
+  }, 20000);
 };
 
 app();
