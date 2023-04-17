40b3fb3b2ea9fe56b12a4965dcecde58f0a481d6
Евгения Лашко <smevlk2014@gmail.com>
2023-04-17 21:01:47 +0300

    fix sound button

--- js/Components/Init/initSound.js
+++ js/Components/Init/initSound.js
@@ -1,9 +1,15 @@
-export const initSound = (button, img) => {
-  const mainAudio = new Audio();
-  mainAudio.src = "../../../public/sounds/main-theme/main-3.mp3";
-  mainAudio.loop = true;
-  mainAudio.volume = 0.2;
+const mainAudio = new Audio();
+mainAudio.src = "../../../public/sounds/main-theme/main-3.mp3";
+mainAudio.loop = true;
+mainAudio.volume = 0.2;
 
+export const initSound = (button, img) => {
+  if (mainAudio.paused) {
+    img.src = "../public/img/icons/sound-off-2.png";
+  } else {
+    img.src = "../public/img/icons/sound-on-2.png";
+  }
+  
   button.addEventListener("click", () => {
     if (mainAudio.paused) {
       img.src = "../public/img/icons/sound-on-2.png";
