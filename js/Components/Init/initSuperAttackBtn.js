acba2e8e8ddcd6404a56a636d26bb009d0006a05
Евгения Лашко <smevlk2014@gmail.com>
2023-04-18 17:59:47 +0300

    done game

--- js/Components/Init/initSuperAttackBtn.js
+++ js/Components/Init/initSuperAttackBtn.js
@@ -1,25 +1,24 @@
 export const initSuperAttackListener = (game, player) => {
-    document.addEventListener("keydown", () => {
-      superAttackListener(event, game, player);
-    });
-  };
-  
-  export const removeSuperAttackListener = () => {
-    document.removeEventListener("keydown", superAttackListener);
-  };
-  
-  const superAttackListener = (event, game, player) => {
-    if (event.code === "Enter") {
-      const isReady = player.superAttack();
-  
-      if (isReady) {
-        game.dealASuperAttack();
+  document.addEventListener("keydown", () => {
+    superAttackListener(event, game, player);
+  });
+};
 
-        const sound = new Audio();
-        sound.src = "../../../public/sounds/sword-sounds/sword-2.mp3";
-        sound.volume = 0.2;
-        sound.play();
-      }
+export const removeSuperAttackListener = () => {
+  document.removeEventListener("keydown", superAttackListener);
+};
+
+const superAttackListener = (event, game, player) => {
+  if (event.code === "Enter") {
+    const isReady = player.superAttack();
+
+    if (isReady) {
+      game.dealASuperAttack();
+
+      const sound = new Audio();
+      sound.src = "../../../public/sounds/sword-sounds/sword-2.mp3";
+      sound.volume = 0.2;
+      sound.play();
     }
-  };
-  
+  }
+};
